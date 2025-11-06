5b16f24765e372d92866a7e4be9edca46f53773b
fadd00 <andhikahutama9@gmail.com>
2025-11-07 02:57:58 +0700

    Refactor MainActivity to include necessary imports for composables; remove unused code in HomePage

--- app/src/main/java/com/sample/prak6/MainActivity.kt
+++ app/src/main/java/com/sample/prak6/MainActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.sample.prak6.ui.theme.Prak6Theme
 
 class MainActivity : ComponentActivity() {
