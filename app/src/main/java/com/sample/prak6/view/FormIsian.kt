2d77b4ed2613763c53e8f748de55903e08765518
fadd00 <andhikahutama9@gmail.com>
2025-11-06 19:40:36 +0700

    Add FormIsian composable with submit button callback

---
+++ app/src/main/java/com/sample/prak6/view/FormIsian.kt
@@ -0,0 +1,16 @@
+package com.sample.prak6
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FormIsian(
+    OnSubmitBtnClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+}
+
