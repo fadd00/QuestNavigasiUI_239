ee584b4ce9eaaa2e39f6ce0f6703a1965968988e
fadd00 <andhikahutama9@gmail.com>
2025-11-06 09:26:27 +0700

    Implement navigation setup in DataApp composable with NavHost and Scaffold

--- app/src/main/java/com/sample/prak6/Navigasi.kt
+++ app/src/main/java/com/sample/prak6/Navigasi.kt
@@ -1,6 +1,9 @@
 package com.sample.prak6
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.internal.composableLambda
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -15,5 +18,20 @@ fun DataApp(
     navController: NavController = rememberNavController(),
     modifier: Modifier
 ){
-    
+    Scaffold { isiRuang->
+        _root_ide_package_.androidx.navigation.NavHost(
+            navController = navController,
+            startDestination = navigasi.Formulir.name,
+            modifier = modifier.padding(isiRuang)
+        ) {
+            composable(route = navigasi.Formulir.name) {
+                FormIsian(
+                    OnSubmitBtnClick = {
+                        navController.navigate(navigasi.Detail.name)
+                    }
+                )
+            }
+            
+        }
+    }
 }
