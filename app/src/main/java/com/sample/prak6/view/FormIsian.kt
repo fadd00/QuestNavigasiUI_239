1bf2ee6ae34b75a37b81b3614be23c8f72448c03
fadd00 <andhikahutama9@gmail.com>
2025-11-07 02:48:37 +0700

    Refactor FormIsian composable to enhance form functionality and layout; add gender and marital status selection

--- app/src/main/java/com/sample/prak6/view/FormIsian.kt
+++ app/src/main/java/com/sample/prak6/view/FormIsian.kt
@@ -1,87 +1,195 @@
 package com.sample.prak6
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FormIsian(
-    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
-    OnSubmitBtnClick: () -> Unit
-) {
+fun FormDataDiri(modifier: Modifier = Modifier) {
+    var textNama by remember { mutableStateOf(value = "") }
+    var textAlamat by remember { mutableStateOf(value = "") }
+    var textJK by remember { mutableStateOf(value = "") }
+    var textSP by remember { mutableStateOf(value = "") }
 
-    Scaffold(modifier = Modifier,
+    var nama by remember { mutableStateOf(value = "") }
+    var alamat by remember { mutableStateOf(value = "") }
+    var jenis by remember { mutableStateOf(value = "") }
+    var status by remember { mutableStateOf(value = "") }
+
+    val gender: List<String> = listOf("Laki-laki", "Perempuan")
+    val statusKawin: List<String> = listOf("Janda", "Lajang", "Duda")
+
+    Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = stringResource(id = R.string.home),
-                    color = Color.White) },
-                colors = TopAppBarDefaults
-                    .topAppBarColors(containerColor = colorResource(id = R.color.teal_700))
+                title = { Text(text = "Formulir Pendaftaran", color = Color.White) },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                )
             )
-        }){
-            isiRuang ->
+        }
+    ) { paddingValues ->
         Column(
-            modifier = Modifier.padding(paddingValues = isiRuang),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(horizontal = 24.dp),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            OutlinedTextField(
-                value = "",
-                singleLine = true,
-                modifier = Modifier
-                    .padding(top = 20.dp)
-                    .width(width = 250.dp),
-                label = { Text(text = "Nama Lengkap") },
-                onValueChange = {},
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "NAMA LENGKAP",
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.align(Alignment.Start)
             )
 
-            HorizontalDivider(
-                modifier = Modifier
-                    .padding(all = 20.dp)
-                    .width(width = 250.dp),
-                thickness = 1.dp,
-                color = Color.Red
+            OutlinedTextField(
+                value = textNama,
+                singleLine = true,
+                shape = MaterialTheme.shapes.large,
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(text = "Nama Lengkap") },
+                onValueChange = {
+                    textNama = it
+                }
             )
-            Row {
-                jenisK.forEach { item ->
-                    Row(verticalAlignment = Alignment.CenterVertically) {
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Text(
+                text = "JENIS KELAMIN",
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.align(Alignment.Start)
+            )
+
+            Column {
+                gender.forEach { item ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .selectable(
+                                selected = textJK == item,
+                                onClick = {
+                                    textJK = item
+                                }
+                            ),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
                         RadioButton(
-                            selected = false,
-                            onClick = { item }
+                            selected = textJK == item,
+                            onClick = {
+                                textJK = item
+                            }
                         )
                         Text(text = item)
                     }
                 }
             }
-            HorizontalDivider(
-                modifier = Modifier
-                    .padding(all = 20.dp)
-                    .width(width = 250.dp),
-                thickness = 1.dp,
-                color = Color.Red
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Text(
+                text = "STATUS PERKAWINAN",
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.align(Alignment.Start)
+            )
+
+            Column(modifier = Modifier.fillMaxWidth()) {
+                statusKawin.forEach { item ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .selectable(
+                                selected = textSP == item,
+                                onClick = { textSP = item }
+                            ),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(
+                            selected = textSP == item,
+                            onClick = { textSP = item }
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Text(
+                text = "ALAMAT",
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.align(Alignment.Start)
             )
 
             OutlinedTextField(
-                value = "",
+                value = textAlamat,
                 singleLine = true,
-                modifier = Modifier
-                    .width(width = 250.dp),
-                label = { Text(text = "Alamat") },
-                onValueChange = {},
+                shape = MaterialTheme.shapes.large,
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(text = "Alamat Lengkap") },
+                onValueChange = {
+                    textAlamat = it
+                }
             )
-            Spacer(modifier = Modifier.height(height = 30.dp))
+
+            HorizontalDivider(
+                modifier = Modifier.padding(
+                    bottom = dimensionResource(R.dimen.padding_medium),
+                    top = dimensionResource(R.dimen.padding_medium)
+                ),
+                thickness = dimensionResource(R.dimen.divider_tipis),
+                color = Color.DarkGray
+            )
+
             Button(
                 modifier = Modifier.fillMaxWidth(fraction = 1f),
-                onClick = OnSubmitBtnClick
+                enabled = textAlamat.isNotEmpty() && textSP.isNotEmpty(),
+                onClick = {
+                    nama = textNama
+                    jenis = textJK
+                    alamat = textAlamat
+                    status = textSP
+                }
             ) {
                 Text(text = stringResource(id = R.string.submit))
+            }
+
+            HorizontalDivider(
+                modifier = Modifier.padding(
+                    bottom = dimensionResource(id = R.dimen.padding_medium),
+                    top = dimensionResource(id = R.dimen.padding_medium)
+                ),
+                thickness = dimensionResource(id = R.dimen.divider_tipis),
+                color = Color.DarkGray
+            )
+
+            ElevatedCard(
+                elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+                colors = CardDefaults.cardColors(containerColor = Color.Black),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp)
+            ) {
+                Column(
+                    modifier = Modifier.padding(horizontal = 5.dp, vertical = 15.dp)
+                ) {
+                    Text(text = "Nama   : $nama", color = Color.White)
+                    Text(text = "Gender : $jenis", color = Color.White)
+                    Text(text = "Status : $status", color = Color.White)
+                    Text(text = "Alamat : $alamat", color = Color.White)
+                }
             }
         }
     }
