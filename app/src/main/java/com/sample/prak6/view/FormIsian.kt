2ac5be5abed3223ad146b7165821e44a672e0a97
fadd00 <andhikahutama9@gmail.com>
2025-11-06 19:43:35 +0700

    Implement layout for FormIsian composable with OutlinedTextField for user input

--- app/src/main/java/com/sample/prak6/view/FormIsian.kt
+++ app/src/main/java/com/sample/prak6/view/FormIsian.kt
@@ -21,5 +21,22 @@ fun FormIsian(
                 colors = TopAppBarDefaults
                     .topAppBarColors(containerColor = colorResource(id = R.color.teal_700))
             )
-}
+}){
+            isiRuang ->
+        Column(
+            modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(width = 250.dp),
+                label = { Text(text = "Nama Lengkap") },
+                onValueChange = {},
+            )
+        }
+    }
 
