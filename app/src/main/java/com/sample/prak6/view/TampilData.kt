25a9f46ac866e834963152c61a036ca0cf5cd0a6
fadd00 <andhikahutama9@gmail.com>
2025-11-06 19:47:10 +0700

    Refactor TampilData to a composable function with a top app bar and item list

--- app/src/main/java/com/sample/prak6/view/TampilData.kt
+++ app/src/main/java/com/sample/prak6/view/TampilData.kt
@@ -1,4 +1,26 @@
 package com.sample.prak6.view
 
-class TampilData {
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    OnBackBtnClick: () -> Unit){
+    val items = listOf(
+        Pair(first = stringResource(id = R.string.nama_lengkap), second = "Contoh Nama"),
+        Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Lainnya"),
+        Pair(first = stringResource(id = R.string.alamat), second = "Yogyakarta")
+    )
+    Scaffold(modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.tampil),
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            )
 }
